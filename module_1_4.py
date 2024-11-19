d287c674ac9c8222a2ff76a5c8a6c5d0dce575c2
Swestly <brat02rus@gmail.com>
2024-11-19 18:41:09 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+my_sting = (input('Ввод в консоль:'))
+print(my_sting.upper()) # // Выведите строку my_string в верхнем регистре.
+print(my_sting.lower()) #  // Выведите строку my_string в нижнем регистре.
+print(my_sting .replace( ' ' , '')) # // Измените строку my_string, удалив все пробелы.
+print(my_sting [0]) # // Выведите первый символ строки my_string.
+print(my_sting[-1])  # // Выведите последний символ строки my_string.
